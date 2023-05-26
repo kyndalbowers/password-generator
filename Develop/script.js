c8412fece44cbe3e478b5c842c9a3adebde088f4
kyndalbowers <131105491+kyndalbowers@users.noreply.github.com>
2023-05-25 21:33:25 -0700

    commenting js file

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,8 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
 
-  //my code
+//my code
+//user input prompts
   var inputPassLength = prompt("What length would you like your password to be? (8-128 characters)")
   if (inputPassLength < 8 || inputPassLength > 128) {
     alert("Length must be between 8 and 128 characters.");
@@ -14,12 +15,14 @@ function generatePassword() {
   var includeNumbers = confirm("Would you like to include numbers?");
   var includeSymbols = confirm("Would you like to include special characters?");
 
+//defining constants
 const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
 const numberOptions = "0123456789";
 const specialCharacters = "`~!@#$%^&*()_-+={}[]:;";
 var selections = [];
 
+//if statements to select characters
 if (includeUpperCase) {
       selections += upperCaseLetters;
   }
@@ -38,15 +41,18 @@ if (includeSymbols) {
 
 console.log(selections);  
 
+//randomize the order of selected characters
 const arr = selections.split("");
 const shuffled = arr.sort(() => 0.5 - Math.random());
 let selected = shuffled.slice(0,inputPassLength);
 
 console.log(selected);
 
+//convert selected characters from array to string
 var newPasscode = selected.join("");
 console.log(newPasscode);
 
+//call function to push generated password to document
 writePassword();
 
 // Write password to the #password input
